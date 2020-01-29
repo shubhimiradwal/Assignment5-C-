05b04f23f87778988f0e8e65b29d94b3507a1160
shubhimiradwal <40629901+shubhimiradwal@users.noreply.github.com>
2020-01-29 17:31:37 +0530

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<int,string>();
+            dictionary.Add(1998,"Atal Bihari Vajpayee");
+            dictionary.Add( 2014,"Narendra Modi");
+            dictionary.Add(2004,"Manmohan Singh");
+
+                string value = dictionary[2004];
+                Console.WriteLine(value);
+                dictionary.Add(2020,"Narendra Modi");
+                dictionary.OrderBy(x => x.Key);
+                foreach (KeyValuePair<int, string> pair in dictionary)
+                 {
+                Console.WriteLine("Key:{0} and Value: {1}",
+                                     pair.Key, pair.Value);
+                }
+        }
+        }
+        }
+    
+
